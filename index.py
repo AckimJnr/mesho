1f86192081bce42a7970cb65e98b16809df1fba5
Ackim Longwe <116080565+AckimJnr@users.noreply.github.com>
2024-10-23 09:56:27 +0200

    Update index.py

--- index.py
+++ index.py
@@ -1,17 +1,21 @@
 import subprocess
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form
 from pydantic import BaseModel
+from twilio.rest import Client
 
 app = FastAPI()
 
 
-class PromptRequest(BaseModel):
-    prompt: str
-
+#class PromptRequest(BaseModel):
+#    data: str
 
 @app.post("/generate/")
-async def generate_text(request: PromptRequest):
-    prompt_text = request.prompt
+async def generate_text(Body: str = Form(...), From: str = Form(...)):
+    prompt_text = Body
+    account_sid = '{AccountSID}'
+    auth_token = '{Account_auth_token}'
+    client = Client(account_sid, auth_token)
+
 
     try:
         # Ollama CLI to generate a response
@@ -24,10 +28,16 @@ async def generate_text(request: PromptRequest):
         )
         # Return the response from the Ollama CLI
         response_text = result.stdout.strip()
+        message = client.messages.create(
+                from_='+18603564557',
+                body=response_text,
+                to=From
+        )
+
     except subprocess.CalledProcessError as e:
         raise HTTPException(status_code=500, detail=f"Error calling Ollama: {e.stderr}")
 
     if not response_text:
         raise HTTPException(status_code=500, detail="Failed to generate response")
 
-    return {"response": response_text}
+    return message
